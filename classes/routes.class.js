d55eef9fa44e6c850d749c2b8177eb34ed12c521
Unknown <frans.herrstrom@gmail.com>
2018-09-23 18:00:56 +0200

    Worked on Schema to assign amount to ingredient of recipes

--- classes/routes.class.js
+++ classes/routes.class.js
@@ -1,6 +1,6 @@
 //propaply singleton sets all express routes
-let RecipesRoute = require("./recipe.class");
-let IngredsRoute = require("./ingredient.class");
+let RecipesRoute = require('./recipe.class');
+let IngredsRoute = require('./ingredient.class');
 
 module.exports = class Routes {
   constructor(app) {
@@ -9,7 +9,7 @@ module.exports = class Routes {
   }
 
   setRoutes() {
-    this.app.get("/allarecept/", (req, res) => {
+    this.app.get('/allarecept/', (req, res) => {
       RecipesRoute.find()
         .then(rec => {
           res.json(rec);
@@ -19,7 +19,7 @@ module.exports = class Routes {
         });
     });
 
-    this.app.get("/allarecept/:recNamn", (req, res) => {
+    this.app.get('/allarecept/:recNamn', (req, res) => {
       let start = req.params.recNamn.toLowerCase();
       // if (start.length < 2) {
       //   res.json({
@@ -30,9 +30,7 @@ module.exports = class Routes {
       RecipesRoute.find()
         .then(rec => {
           let result = rec
-            .filter(
-              recipe => recipe.Name.toLowerCase().indexOf(start) == 0
-            )
+            .filter(recipe => recipe.Name.toLowerCase().indexOf(start) == 0)
             .map(recipe => recipe);
           res.json(result);
           console.log(result);
@@ -42,16 +40,19 @@ module.exports = class Routes {
         });
     });
 
+    this.app.get('/allaingreds/', (req, res) => {
+      IngredsRoute.find()
+        .then(rec => {
+          res.json(rec.splice(0, 5));
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    });
 
     //Får ut ingredienser från Namn string
-    this.app.get("/allaingreds/:ingNamn", (req, res) => {
+    this.app.get('/allaingreds/:ingNamn', (req, res) => {
       let start = req.params.ingNamn.toLowerCase();
-      if (start.length < 2) {
-        res.json({
-          error: "please provide at least 2 characters"
-        });
-        return;
-      }
       IngredsRoute.find()
         .then(rec => {
           let result = rec
@@ -59,7 +60,7 @@ module.exports = class Routes {
               ingredient => ingredient.Namn.toLowerCase().indexOf(start) == 0
             )
             .map(ingredient => ingredient);
-          res.json(result);
+          res.json(result.splice(0, 10));
           console.log(result);
         })
         .catch(err => {
@@ -67,24 +68,30 @@ module.exports = class Routes {
         });
     });
 
-//Lägga till i databasen fungerar som nedan
+    //Lägga till i databasen fungerar som nedan
 
-//   var Köttbullar = new RecipesRoute({
-//       Name: 'Köttbullar',
-//       Description: 'Hemmalagade',
-//       Ingredients: [{
-//           Namn: 'Köttfärs',
-//           halsovarden: 500
-//       }, {
-//           Namn: 'lök',
-//           halsovarden: 100
-//       },{
-//           Namn:'Tomat',
-//           halsovarden:1000
-//       }],
-//       IMGUrl:'https://goo.gl/images/z1tbYf'
-//   });
-
-// Köttbullar.save();
+    var Nudlar = new RecipesRoute({
+      Name: 'Nudlar',
+      Description: 'Nudlar med gräslök',
+      Ingredients: [
+        {
+          Ingredient: {
+            Namn: 'Nudlar',
+            halsovarden: 500
+          },
+          Amount: 1112
+        },
+        {
+          Ingredient: {
+            Namn: 'Gräslök',
+            halsovarden: 500
+          },
+          Amount: 555
+        },
+      ],
+      IMGUrl:
+        'https://s-media-cache-ak0.pinimg.com/originals/87/94/29/8794292e4b7a2091ce8dfb7f236279c7.jpg'
+    });
+    Nudlar.save();
   }
 };
